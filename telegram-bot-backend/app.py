982c505a2755225ef52527d96d2fa18ec55b9d6b
Divyansh <divyanshji1707@gmail.com>
2024-08-14 14:01:38 +0530

    added backend code for telegram setup

--- telegram-bot-backend/app.py
+++ telegram-bot-backend/app.py
@@ -13,3 +13,72 @@ def get_video():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+# from flask import Flask, jsonify, request
+# from flask_cors import CORS
+# import telebot
+
+# # Flask setup
+# app = Flask(__name__)
+# CORS(app)
+
+# # Telegram bot setup
+# API_TOKEN = 'YOUR_TELEGRAM_BOT_API_TOKEN'
+# bot = telebot.TeleBot(API_TOKEN)
+
+# video_data = {
+#     'ad_video_url': 'https://www.youtube.com/embed/rXzfcLIzGQU',
+#     'main_video_url': 'https://www.youtube.com/embed/51QV0v8GGbM'
+# }
+
+
+# @bot.message_handler(commands=['start'])
+# def send_welcome(message):
+#     bot.send_message(
+#         message.chat.id,
+#         "Welcome! Click the link to watch an ad. After skipping, the main video will start."
+#     )
+#     send_ad(message)
+
+# def send_ad(message):
+#     markup = telebot.types.InlineKeyboardMarkup()
+#     skip_button = telebot.types.InlineKeyboardButton(text="Skip Ad", callback_data="skip")
+#     close_button = telebot.types.InlineKeyboardButton(text="Close Ad", callback_data="close")
+#     markup.add(skip_button, close_button)
+
+#     bot.send_message(
+#         message.chat.id,
+#         f"Watch this ad: {video_data['ad_video_url']}",
+#         reply_markup=markup
+#     )
+
+
+# @bot.callback_query_handler(func=lambda call: call.data in ["skip", "close"])
+# def handle_callback(call):
+#     if call.data == "skip":
+#         bot.send_message(
+#             call.message.chat.id,
+#             f"Ad skipped! Now playing: {video_data['main_video_url']}"
+#         )
+#     elif call.data == "close":
+#         bot.send_message(
+#             call.message.chat.id,
+#             "Ad closed. No video will be played."
+#         )
+
+#     bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id, reply_markup=None)
+
+# @app.route('/webhook', methods=['POST'])
+# def webhook():
+#     json_str = request.get_data().decode('UTF-8')
+#     update = telebot.types.Update.de_json(json_str)
+#     bot.process_new_updates([update])
+#     return 'OK', 200
+
+# if __name__ == '__main__':
+#     bot.remove_webhook()
+#     # Have to use ngrok url
+#     bot.set_webhook(url="https://localhost:4200/webhook")
+#     app.run(debug=True)
+
